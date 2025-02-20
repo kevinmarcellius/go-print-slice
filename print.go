4e2ff84d2b304a34e1dac1c8e255611efe5269a8
kevinmarcellius <kevin.marcellius@ui.ac.id>
2025-02-20 13:02:51 +0700

    chore: use tab for formatting

--- print.go
+++ print.go
@@ -6,14 +6,14 @@ import (
 
 func PrintSlice(slice []int) {
 	for _, v := range slice {
-		fmt.Println(v)
+		fmt.Print(v, "\t")
 	}
 }
 
 func Print2DSlice(slice [][]int) {
 	for i := range slice {
 		for j := range slice[i] {
-			fmt.Print(slice[i][j], " ")
+			fmt.Print(slice[i][j], "\t")
 		}
 		fmt.Println()
 	}
